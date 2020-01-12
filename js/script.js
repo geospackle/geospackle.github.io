d79c090819800ec1d7f8ff4ae1e7571dae498d6a
Antti <antti.molsa@gmail.com>
2020-01-12 12:37:41 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,9 +3,4 @@ $(document).ready(function () {
     $('html, body').animate({
         scrollTop: $('#scrollhere').offset().top
     }, 1000)
-}.then(function () {
-    $('html, body').animate({
-        scrollTop: $('#scrollhere2').offset().top
-    }, 3000)
 })
-)
